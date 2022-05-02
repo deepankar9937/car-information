b96ce3a7b78e5f963507607dd072c46e28e2606a
deepankar <deepankarbehera96@gmail.com>
2022-05-02 15:12:24 +0530

    batch update

--- learningmanagement/src/main/java/com/te/lms/controller/BatchController.java
+++ learningmanagement/src/main/java/com/te/lms/controller/BatchController.java
@@ -3,6 +3,7 @@ package com.te.lms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,11 @@ public class BatchController {
 		} else {
 			return new ResponseEntity<String>("No Data Found",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<?> deleteDetails(@PathVariable String empId) {
+		return null;
+		
 	}
 }
